6f5811d0c5871e2ab883d64dc19be642942a1544
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-09-14 11:38:32 -0700

    gather user inputs

--- src/app.ts
+++ src/app.ts
@@ -39,10 +39,36 @@ class ProjectInput {
         this.mount();
     }
 
+    private gatherInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        if(enteredTitle.trim().length === 0 ||
+        enteredDescription.trim().length === 0 ||
+        enteredPeople.trim().length === 0) {
+            alert("Invalid input, please try again!");
+            return;
+        } else {
+            return[enteredTitle, enteredDescription, +enteredPeople]
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = "";
+        this.descriptionInputElement.value = "";
+        this.peopleInputElement.value = "";
+    }
+
     @autoBind
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value)
+        const userInput = this.gatherInput();
+        if(Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people)
+        };
+        this.clearInputs();
     }
 
     private configure() {
