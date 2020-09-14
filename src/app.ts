486b4f55f392eee0eefe878a786c4e57a0dcd2ad
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-09-14 15:22:36 -0700

    project list item

--- src/app.ts
+++ src/app.ts
@@ -160,6 +160,13 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
     private project: Project;
 
+    get persons() {
+        if(this.project.people === 1) {
+            return `1 person`;
+        } else {
+            return `${this.project.people} persons`
+        }
+    }
     constructor(hostId: string, project: Project) {
         super("single-project", hostId, false, project.id);
         this.project = project;
@@ -171,7 +178,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
     configure() {}
     renderContent() {
         this.element.querySelector('h2')!.textContent = this.project.title;
-        this.element.querySelector('h3')!.textContent = `Team of ${this.project.people.toString()}`;
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
         this.element.querySelector('p')!.textContent = this.project.description;
     }
 }
@@ -216,7 +223,7 @@ class ProjectList extends Component <HTMLDivElement, HTMLElement> {
     )! as HTMLUListElement;
     listEl.innerHTML = "";
     for (const prjItem of this.assignedProjects) {
-        new ProjectItem(this.element.id, prjItem);
+        new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
     }
   }
 }
