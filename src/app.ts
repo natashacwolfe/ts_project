4782fcfbddff1238b7400886094daa7fa444536b
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-09-14 11:08:56 -0700

    project input render form

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
 // Code goes here!
 
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLElement;
+    element: HTMLFormElement;
 
+    constructor() {
+        this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
+        this.hostElement = document.getElementById("app")!;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.mount();
+    }
+
+    private mount() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    }
+}
+
+
+const prjInput = new ProjectInput();
